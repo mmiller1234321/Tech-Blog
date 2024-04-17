091a853a1f9142b7390c347ebd983bcc8cf891a8
mmiller1234321 <148365315+mmiller1234321@users.noreply.github.com>
2024-04-17 12:45:56 -0500

    update

--- config/connection.js
+++ config/connection.js
@@ -3,17 +3,18 @@ require('dotenv').config();
 
 let sequelize;
 
+
 if (process.env.DB_URL) {
   sequelize = new Sequelize(process.env.DB_URL);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_PW,
     {
       host: 'localhost',
-      dialect: 'postgres'
-    }
+      dialect: 'postgres',
+    },
   );
 }
 
