3573e18a47a8af09873ae4938e2dbaf6c065f109
mmiller1234321 <148365315+mmiller1234321@users.noreply.github.com>
2024-04-17 09:18:27 -0500

    update

--- config/connection.js
+++ config/connection.js
@@ -6,15 +6,10 @@ let sequelize;
 if (process.env.DB_URL) {
   sequelize = new Sequelize(process.env.DB_URL);
 } else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'dpg-coft7qol5elc73dkg010-a.oregon-postgres.render.com',
-      dialect: 'postgres'
-    }
-  );
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    dialect: 'postgres',
+    host: 'dpg-coft7qol5elc73dkg010-a', // Update the host with your Render PostgreSQL hostname
+  });
 }
 
 module.exports = sequelize;
