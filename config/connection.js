76cb06cab7957959791595fd0fddc5835f0a910e
mmiller1234321 <148365315+mmiller1234321@users.noreply.github.com>
2024-04-17 12:14:52 -0500

    render

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,16 @@ let sequelize;
 if (process.env.DB_URL) {
   sequelize = new Sequelize(process.env.DB_URL);
 } else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    dialect: 'postgres',
-    host: 'dpg-co5cc60l6cac73ddm8ig-a', // Update the host with your Render PostgreSQL hostname
-    port: 5432, // Update the port if necessary
-  });
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'postgres'
+    }
+  );
 }
 
 module.exports = sequelize;
+
